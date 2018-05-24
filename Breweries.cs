a352aca1fc373aaa5bab0cdda08045ceaed8c03b
Vygantas <vygantasskirius@gmail.com>
2018-05-24 22:35:28 +0300

    init

---
+++ Breweries.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDog
+{
+    class Breweries
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Adress1 { get; set; }
+        public string Adress2 { get; set; }
+        public string City { get; set; }
+
+        public Breweries()
+        {
+        }
+
+        public Breweries(int id, string name, string adress1, string adress2, string city)
+        {
+            Id = id;
+            Name = name;
+            Adress1 = adress1;
+            Adress2 = adress2;
+            City = city;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DataDog
+{
+    class Program
+    {
+        List<Breweries> Breweries = new List<Breweries>();
+        static void Main(string[] args)
+        {
+            ReadBreweries("https://raw.githubusercontent.com/brewdega/open-beer-database-dumps/master/dumps/breweries.csv");
+        }
+        public static List<Breweries> ReadBreweries(string location)
+        {
+            WebClient client = new WebClient();
+            List<Breweries> breweries = new List<Breweries>();
+            string file = client.DownloadString(location);
+            string[] lines = file.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+            foreach (string line in lines)
+            {
+                line.Replace('\"', '"');
+                Console.WriteLine(line);
+                string[] data = line.Split(',');
+            }
+            //using(StreamReader reader = new StreamReader(@location))
+            //{
+            //    string line = "";
+            //    while((line = reader.ReadLine()) != null)
+            //    {
+            //        string[] data = line.Split(',');
+            //    }
+            //}
+            return breweries;
+        }
+    }
+}
